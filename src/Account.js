5775f45055b48ab6ac4bdc02ceded83d0c8401f9
addykumar27 <adarshna27@yahoo.com>
2017-05-01 20:18:09 -0700

    adding deposit and withdraw function to checking and saving

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(props) {
+   super(props);
+   this.state= {
+    balance: 0
+   }
+ }
+
+handleDepositClick (event) {
+  event.preventDefault()
+  let cash = parseInt(this.refs.money.value);
+  let newCash= this.state.balance + cash;
+ this.setState ({
+  balance: newCash
+ }) 
+console.log(this.refs.money.value);
+}
+
+handleWithdrawalClick(event) {
+event.preventDefault()
+let cash = parseInt(this.refs.money.value);
+let newCash = this.state.balance - cash;
+this.setState ({
+  balance: newCash
+})
+console.log(this.refs.money.value);
+}
+
+
+
   render() {
+  let balanceClass ="balance"
+  if(this.state.balance === 0) {
+    balanceClass += " zero";
+  }
+
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balanceClass">${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref="money"/>
+        <input type="button" value="Deposit" onClick={(event) => this.handleDepositClick(event)}/>
+        <input type="button" value="Withdraw"onClick={(event) => this.handleWithdrawalClick(event)}/>
+       
       </div>
     )
   }
--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import logo from './ga.png';
 import Account from './Account';
 
 class App extends Component {
+ 
+
   render() {
     return (
       <div id="content">
@@ -11,8 +13,10 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account />
-        <Account />
+        <div>
+        <Account name="Checking"/>
+        <Account name="Savings"/>
+        </div>
         <div className="clear"></div>
       </div>
     );
